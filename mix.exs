0bfafe8b44dd14997ca99e171f8a29ffbd7cd5ff
pauloancheta <paulo.ancheta@gmail.com>
2016-09-05 19:14:54 -0700

    add documentation

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.Mixfile do
      escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.3",
+     name: "Issues",
+     source_url: "https://github.com/pauloancheta/issues",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -30,7 +32,9 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.9"},
-      {:poison, "~> 1.5"}
+      {:poison, "~> 1.5"},
+      {:ex_doc, "~> 0.11"},
+      {:earmark, ">= 0.0.0"},
     ]
   end
 
